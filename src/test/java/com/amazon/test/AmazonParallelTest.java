87d2d595f7b28ab2c079b2b70cb633fdb793060e
Padmini0707 <mspuranik1999@gmail.com>
2025-12-23 18:34:23 +0530

    final commit for jenkins

--- src/test/java/com/amazon/test/AmazonParallelTest.java
+++ src/test/java/com/amazon/test/AmazonParallelTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class AmazonParallelTest {
-	
+	//
 	static WebDriver driver=null;
 	
 	@Parameters({"bname"})
